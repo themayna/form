ca6f8f21af4a3313bf65a59a978390c899cd985f
mainericiangel <P@rola1234>
2017-07-05 00:00:19 +0300

    changed my mind

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,20 +5,34 @@ namespace AppBundle\Entity;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
+
 /**
+ * Class User
+ * @package AppBundle\Entity
  * @ORM\Entity
- * @ORM\Table(name="users")
+ * @ORM\Table(name="user")
  */
-class User extends BaseUser {
+class User extends BaseUser
+{
 	/**
+	 * @var int
 	 * @ORM\Id
 	 * @ORM\Column(type="integer")
 	 * @ORM\GeneratedValue(strategy="AUTO")
 	 */
 	protected $id;
 
-	public function __construct() {
+
+	public function __construct ()
+	{
 		parent::__construct();
-		// your own logic
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getId ()
+	{
+		return $this->id;
 	}
 }
